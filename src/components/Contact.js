2f2c0d1414e7b48a42c43033bd28fb3689950596
RubinVelcani <rubin.velcani@gmail.com>
2021-06-15 15:58:23 +0200

    'focus rings'

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,12 +31,12 @@ const Contact = () => {
         <h1 className='text-2xl font-bold mb-14'>Get in touch or write me an email!</h1>
         <div className='flex flex-col justify-center md:flex-row'>
           <div className='flex flex-col items-center mb-10 md:mr-20'>
-            <img className='w-10' src='../../images/email.png' alt='' />
+            <img className='w-10' src='../../images/email.png' alt=''/>
             <h1 className='text-xl text-gray-600 mt-2'>Email:</h1>
             <h1 className='text-lg font-semibold'>rubin.velcani@gmail.com</h1>
           </div>
           <div className='flex flex-col items-center mb-10'>
-            <img className='w-10' src='../../images/phone.png' alt='' />
+          <img className='w-10' src='../../images/phone.png' alt=''/>
             <h1 className='text-xl text-gray-600 mt-2'>Phone:</h1>
             <h1 className='text-lg font-semibold'>+355694629009</h1>
           </div>
@@ -46,16 +46,16 @@ const Contact = () => {
             <div className='w-4/5 max-w-screen-sm flex flex-col text-left text-formGray mt-10 mb-16 mx-auto'>
               <input type="hidden" name="form-name" value="rubin" />
               <label className='text-lg mr-auto mb-5'>Full Name</label>
-              <input value={name} onChange={(e) => setName(e.target.value)} type='name' name='fullName' className='  ring-1 ring-gray-400 mb-4 py-4 px-4 outline-none focus-within:ring-blue-400' placeholder='Name' />
+              <input value={name} onChange={(e) => setName(e.target.value)} type='name' name='fullName' className='ring-1 ring-lightGray focus:ring-mainYellow mb-4 py-4 px-4 outline-none focus-within:ring-blue-400' placeholder='Name' />
               {nameErr && <h3 className='text-red-400 pb-1 mb-10'>Please type a valid name</h3>}
               <label className='text-lg mr-auto mb-5'>Email Address</label>
-              <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' name='email' className='  ring-1 ring-gray-400 mb-8 py-4 px-4 outline-none focus-within:ring-blue-400' placeholder='Email' />
+              <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' name='email' className='ring-1 ring-lightGray focus:ring-mainYellow mb-8 py-4 px-4 outline-none focus-within:ring-blue-400' placeholder='Email' />
               {emailErr && <h3 className='text-red-400 pb-1 mb-10'>Please type a valid email address</h3>}
               <label className='text-lg mr-auto mb-5'>Your Message</label>
-              <textarea value={message} onChange={(e) => setMessage(e.target.value)} name='message' className='  ring-1 ring-gray-400 mb-5 py-3 px-6 outline-none focus-within:ring-blue-400' placeholder='Your message...' />
+              <textarea value={message} onChange={(e) => setMessage(e.target.value)} name='message' className='ring-1 ring-lightGray focus:ring-mainYellow mb-5 py-3 px-6 outline-none focus-within:ring-blue-400' placeholder='Your message...' />
               {messageErr && <h3 className='text-red-400 mb-10'>Please type a valid message</h3>}
               <div className='flex flex-row justify-around mt-10'>
-                <button onClick={validateForm} className='w-1/2  bg-mainYellow text-black text-lg ring-1 ring-gray-400 mx-auto p-3 cursor-pointer outline-none focus-within:ring-blue-400 md:w-5/12'>Submit </button>
+                <button onClick={validateForm} className='w-1/2  bg-mainYellow text-black text-lg ring-1 ring-lightGray focus:ring-mainYellow mx-auto p-3 cursor-pointer outline-none focus-within:ring-blue-400 md:w-5/12'>Submit </button>
               </div>
             </div>
           </div>
