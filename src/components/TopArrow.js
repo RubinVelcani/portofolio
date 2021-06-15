91097af6e4adf44d521181677af2fe3b505f6acd
RubinVelcani <rubin.velcani@gmail.com>
2021-06-15 16:19:21 +0200

    'fixed arro'

--- src/components/TopArrow.js
+++ src/components/TopArrow.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TopArrow = () => (
-  <a className='fixed bg-mainYellow bottom-5 right-5 p-2' href='#home' style={{clipPath: 'circle(50% at 50% 50%)'}}>
+  <a className='fixed bg-mainYellow bottom-5 right-5 p-2 lg:bottom-10 lg:right-10' href='#home' style={{clipPath: 'circle(50% at 50% 50%)'}}>
     <img className='bg-white' src='../../images/arrow-top.svg' alt='' style={{clipPath: 'circle(50% at 50% 50%)'}}/>
   </a>
 )
