41036e0d7ac4da3da1c2975a6c6f6881edcbd462
RubinVelcani <60612243+RubinVelcani@users.noreply.github.com>
2021-07-03 08:59:34 +0200

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,13 +5,19 @@ const Project = ({ title, image, site, description, live }) => {
   return (
     <section className='relative mb-10'>
       <h1 className='text-2xl font-semibold mb-5'>{title}</h1>
-      <a href={live ? site : ''}
-        disabled={live}
+{ live ? 
+  (
+  <a href={site}
         target='_blank'
         rel='noreferrer noopener'
       >
         <img src={image} alt=''/>
       </a>
+ ) : 
+ (
+          <img src={image} alt=''/>
+          )
+}
       <h2 className='text-lg mt-5'>{description}</h2>
     </section>
   )
